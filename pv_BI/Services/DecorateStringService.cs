5faf21a9cdfe2cb17d220a7c83d94e538a9d3dcb
Alex Kozlowski <akozlowski@shiftgig.com>
2018-02-15 21:34:00 -0600

    cleaning up project

--- pv_BI/Services/DecorateStringService.cs
+++ pv_BI/Services/DecorateStringService.cs
@@ -9,11 +9,6 @@ namespace pv_BI.Services
 {
     public class DecorateStringService : IDecorateStringService
     {
-        private readonly IPatientRegistrationService _patientRegistrationService;
-        public DecorateStringService(IPatientRegistrationService patientRegistrationService)
-        {
-            _patientRegistrationService = patientRegistrationService;
-        }
         public string Formater(IEnumerable<string> elements)
         {
             return String.Join(System.Environment.NewLine, elements);
