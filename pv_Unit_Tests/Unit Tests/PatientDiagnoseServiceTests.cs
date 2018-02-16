d6924b9692b6c1f113758166d487c1d3e4c87eb3
Alex Kozlowski <akozlowski@shiftgig.com>
2018-02-15 21:22:46 -0600

    added unit tests

---
+++ pv_Unit_Tests/Unit Tests/PatientDiagnoseServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using pv_BI.Services;
+
+namespace pv_Unit_Tests.Unit_Tests
+{
+    public class PatientDiagnoseServiceTests
+    {
+        private Mock<PatientDiagnoseService> _mockPatientDiagnoseService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockPatientDiagnoseService = new Mock<PatientDiagnoseService>();
+
+        }
+
+        [TestCase(1, ExpectedResult = "1")]
+        [TestCase(2, ExpectedResult = "Diagnose")]
+        [TestCase(7, ExpectedResult = "Patient")]
+        [TestCase(14, ExpectedResult = "Diagnose Patient")]
+        public string ReplaceTests(int input)
+        {
+            var inputList = new List<int>() { input };
+            return _mockPatientDiagnoseService.Object.Replace(inputList).FirstOrDefault();
+        }
+    }
+}
---
+++ pv_Unit_Tests/Unit Tests/PatientRegisterServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using pv_BI.Interfaces;
+using pv_BI.Services;
+
+namespace pv_Tests.Tests
+{
+    public class PatientRegisterServiceTests
+    {
+
+        private Mock<PatientRegistrationService> _mockPatientRegistrationService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockPatientRegistrationService = new Mock<PatientRegistrationService>();
+
+        }
+
+        [TestCase(1, ExpectedResult = "1")]
+        [TestCase(3, ExpectedResult = "Register")]
+        [TestCase(5, ExpectedResult = "Patient")]
+        [TestCase(15, ExpectedResult = "Register Patient")]
+        public string ReplaceTests(int input)
+        {
+            var inputList = new List<int>(){input};
+            return _mockPatientRegistrationService.Object.Replace(inputList).FirstOrDefault();
+        }
+    }
+}
